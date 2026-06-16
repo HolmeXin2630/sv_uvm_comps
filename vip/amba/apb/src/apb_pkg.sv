f9a2eb5c672bf6b365e0060446b828d61c4fc922
HolmeXin2630 <hx1076260997@outlook.com>
2026-06-17 00:15:28 +0800

    feat(apb): create directory structure and scaffolding

---
+++ vip/amba/apb/src/apb_pkg.sv
@@ -0,0 +1,15 @@
+`ifndef APB_PKG_SV
+`define APB_PKG_SV
+
+package hx_apb_pkg;
+    import uvm_pkg::*;
+    `include "uvm_macros.svh"
+
+    // TODO: `include 所有组件
+    // `include "apb_defines.svh"
+    // `include "apb_transaction.sv"
+    // ...
+
+endpackage
+
+`endif // APB_PKG_SV
---
+++ vip/amba/apb/tb/apb_tb_pkg.sv
@@ -0,0 +1,16 @@
+`ifndef APB_TB_PKG_SV
+`define APB_TB_PKG_SV
+
+package hx_apb_tb_pkg;
+    import uvm_pkg::*;
+    import hx_apb_pkg::*;
+    `include "uvm_macros.svh"
+
+    // TODO: `include tb 和 test 文件
+    // `include "apb_slave_ram.sv"
+    // `include "apb_base_test.sv"
+    // ...
+
+endpackage
+
+`endif // APB_TB_PKG_SV
